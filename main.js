75d9d4ccc05ed81ccdccae9c30ea0518a4351317
Steven Adler <NarturArkano@users.noreply.github.com>
2021-07-02 12:36:32 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+    // include the Node.js 'path' module at the top of your file
+const path = require('path')
+
+// modify your existing createWindow() function
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true
+        }
+    })
+
+    win.loadFile('index.html')
+}
+// ...
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', function() {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') app.quit()
+})
---
+++ readme.md
@@ -0,0 +1,10 @@
+Windows 10 64-bit
+
+# Clone this repository
+git clone https://github.com/NarturArkano/StevenAdler.ElectronPortfolio
+# Go into the repository
+cd StevenAdler.ElectronPortfolio
+# Install dependencies
+npm install
+# Run the app
+npm start
